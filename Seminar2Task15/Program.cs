ebb2588bc5160b132bac77c96ee9f977f43c70ee
Sveta <nostress58@ya.ru>
2022-12-08 10:22:02 +0300

    add solution fot task15

--- Seminar2Task15/Program.cs
+++ Seminar2Task15/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите день недели: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if ((number > 0) && (number < 8))
+{
+    Dictionary<int, string> dict = new Dictionary<int, string>
+    {
+        {1, "рабочий день"},
+        {2, "рабочий день"},
+        {3, "рабочий день"},
+        {4, "рабочий день"},
+        {5, "рабочий день"},
+        {6, "выходной день"},
+        {7, "выходной день"},
+    };
+    Console.WriteLine(dict[number]);
+}
+else
+Console.WriteLine("Вы ввели не день недели");
