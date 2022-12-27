9ad25154bc854f1e9db52c67bf65592186308eae
Sveta <nostress58@ya.ru>
2022-12-27 10:00:53 +0300

    add solution for task 64,66,68

---
+++ Seminar9Task66/Program.cs
@@ -0,0 +1,33 @@
+﻿//ПрограммА находит сумму натуральных элементов в промежутке от M до N. Рекурсия
+
+int numM = ReadData("Введите число M ");
+int numN = ReadData("Введите число N ");
+int result = SumMN(numM, numN);
+PrintData("Сумма элменетов равна: "+result);
+
+//Метод поиска суммы чисел от M до N
+int SumMN(int M, int N)
+{
+    if (M>=N ) 
+    {
+        return N;
+    }
+    else
+    {
+              return M+SumMN(M+1,N);
+    }
+}
+
+// Метод вывода данных
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Метод считывания данных от пользователя
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
---
+++ Seminar9Task68/Program.cs
@@ -0,0 +1,32 @@
+﻿// Программа вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int numM = ReadData("Введите число M ");
+int numN = ReadData("Введите число N ");
+int result = AkkermanF(numM, numN);
+PrintData($"Функция Аккермана для чисел ({numM},{numN}) = "+result);
+
+// Функция Аккермана
+int AkkermanF(int m, int n)
+{
+  if (m == 0)
+    return n + 1;
+  else
+    if ((m != 0) && (n == 0))
+      return AkkermanF(m - 1, 1);
+    else
+      return AkkermanF(m - 1, AkkermanF(m, n - 1));
+}
+
+// Метод вывода данных
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+// Метод считывания данных от пользователя
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
